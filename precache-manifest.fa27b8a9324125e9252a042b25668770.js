890003c6dfb33f03bbff29191581edc7e9e50e42
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-06-25 16:49:50 +0300

    Updates

---
+++ precache-manifest.fa27b8a9324125e9252a042b25668770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30dec408ce5ab89dcd86aead9d9fc70",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "35a5f9988e3fcc2f4763",
+    "url": "/react_redux-list-of-todos/static/css/main.552c3d31.chunk.css"
+  },
+  {
+    "revision": "571c3a24e3947367ffa3",
+    "url": "/react_redux-list-of-todos/static/js/2.25e08047.chunk.js"
+  },
+  {
+    "revision": "35a5f9988e3fcc2f4763",
+    "url": "/react_redux-list-of-todos/static/js/main.0650beeb.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
